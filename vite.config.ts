43dc6785fad8716c9831e829143641b7bcbe17b9
Mohammed Hamraz <hamraz8055@gmail.com>
2026-03-19 16:36:54 +0300

    fix: base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/marcazi-saudi-connect/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
